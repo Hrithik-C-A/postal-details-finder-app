8caf422af83d678bd341acca97c791908d51c4f2
Hrithik C A <hrithikca@gmail.com>
2023-06-16 14:19:19 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className='container'>
       {/* <DarkMode buttonSize={32} buttonCss={'darkmode-button'}/> */}
       <img src="https://media1.giphy.com/media/C0o4OhvZk3SNKzL0iU/giphy.gif?cid=ecf05e47ref0562df50yl7l1zvielwv2jjx4zvbfk6hr0vsr&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="postbox image" />
-      <h1>Postal Deatils Finder App</h1>
+      <h1>Postal Details Finder App</h1>
         <div className='link-container'>
           <a href='/search/pincode' className='link pincode-link'>Search By Pincode</a>
           <a href='/search/branchname' className='link branch-link'>Search By BranchName</a>
