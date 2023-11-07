ba13594b15c7ce2b376cc209206ed81a3819a8c4
Hrithik C A <hrithikca@gmail.com>
2023-11-07 10:37:09 +0530

    removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import DarkMode from './components/DarkMode'
 const App = () => {
   return (
     <div className='container'>
-      {/* <DarkMode buttonSize={32} buttonCss={'darkmode-button'}/> */}
       <img src="https://media1.giphy.com/media/C0o4OhvZk3SNKzL0iU/giphy.gif?cid=ecf05e47ref0562df50yl7l1zvielwv2jjx4zvbfk6hr0vsr&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="postbox image" />
       <h1>Postal Details Finder App</h1>
         <div className='link-container'>
